4722db828b7499cfe2870610ec1d05188c77535d
Hank Greene <hg8026@tgndomains.com>
2022-12-23 18:54:33 +0000

    commit post power outage

--- consumer/app/src/main/java/io/crtp/bbmnt/consumer/App.java
+++ consumer/app/src/main/java/io/crtp/bbmnt/consumer/App.java
@@ -3,6 +3,13 @@
  */
 package io.crtp.bbmnt.consumer;
 
+import java.time.Duration;
+import java.util.Properties;
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,5 +17,34 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        //Kafka consumer configuration settings
+        //String topicName = args[0].toString();
+        String topicName = "kafkaDev";
+        Properties props = new Properties();
+        //props.put("bootstrap.servers", "localhost:9092");
+        props.put("bootstrap.servers", "10.10.89.95:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer",
+            "org.apache.kafka.common.serializa-tion.StringDeserializer");
+        props.put("value.deserializer",
+            "org.apache.kafka.common.serializa-tion.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer
+            <String, String>(props);
+            //Kafka Consumer subscribes list of topics here.
+        consumer.subscribe(Arrays.asList(topicName));
+        //print the topic name
+        System.out.println("Subscribed to topic " + topicName);
+        int i = 0;
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(60000));
+            for (ConsumerRecord<String, String> record : records)
+            // print the offset,key and value for the consumer records.
+            System.out.printf("offset = %d, key = %s, value = %s\n",
+            record.offset(), record.key(), record.value());
+        }
     }
 }
